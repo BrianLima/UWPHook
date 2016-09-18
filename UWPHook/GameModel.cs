f38060e1ad15e84911e0ba19d767cfccfab5acdd
Brian Lima <brian.ghwt@outlook.com>
2016-09-18 15:39:25 -0300

    Cleaning

--- UWPHook/GameModel.cs
+++ UWPHook/GameModel.cs
@@ -1,15 +1,7 @@
 ﻿using Newtonsoft.Json;
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Data;
-using UWPHook.Properties;
 
 namespace UWPHook
 {
