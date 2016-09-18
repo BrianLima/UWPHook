3faa422c8e9bd4b5ca4ee432bd3a0f20be0f9cdf
Brian Lima <brian.ghwt@outlook.com>
2016-09-18 12:14:45 -0300

    Base implemented

---
+++ UWPHook/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UWPHook
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<GameModel> games;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            if (Environment.GetCommandLineArgs() != null)
+            {
+            }
+
+            games = new List<GameModel>();
+            for (int i = 0; i < 10; i++)
+            {
+                games.Add(new GameModel { game_alias = "sajufhsaduifhuisdsdgbuigduisaguidsguisaguiasguidasg", game_path = "sajufhsaduifhuisdsdgbuigduisaguidsguisaguiasguidasgx" });
+            }
+            
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            listView.ItemsSource = games;
+            
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            games.Add(new GameModel { game_alias = alias_textBox.Text, game_path = path_textBox.Text });
+        }
+    }
+}
