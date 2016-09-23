ec3bb06768aeff95b2e57f9da56aa2ae5a5e62c7
Brian Lima <briano.dev@outlook.com>
2016-09-23 09:25:51 -0300

    Working on bugs with Forza Horizon

--- UWPHook/ProcessManager.cs
+++ UWPHook/ProcessManager.cs
@@ -1,12 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace UWPHook
 {
-    class ProccessManager
+    public enum ActivateOptions
     {
+        None = 0x00000000,  // No flags set
+        DesignMode = 0x00000001,  // The application is being activated for design mode, and thus will not be able to
+                                  // to create an immersive window. Window creation must be done by design tools which
+                                  // load the necessary components by communicating with a designer-specified service on
+                                  // the site chain established on the activation manager.  The splash screen normally
+                                  // shown when an application is activated will also not appear.  Most activations
+                                  // will not use this flag.
+        NoErrorUI = 0x00000002,  // Do not show an error dialog if the app fails to activate.
+        NoSplashScreen = 0x00000004,  // Do not show the splash screen when activating the app.
+    }
+
+    [ComImport, Guid("2e941141-7f97-4756-ba1d-9decde894a3d"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    interface IApplicationActivationManager
+    {
+        // Activates the specified immersive application for the "Launch" contract, passing the provided arguments
+        // string into the application.  Callers can obtain the process Id of the application instance fulfilling this contract.
+        IntPtr ActivateApplication([In] String appUserModelId, [In] String arguments, [In] ActivateOptions options, [Out] out UInt32 processId);
+        IntPtr ActivateForFile([In] String appUserModelId, [In] [MarshalAs(UnmanagedType.Interface, IidParameterIndex = 2)] /*IShellItemArray* */ IShellItemArray itemArray, [In] String verb, [Out] out UInt32 processId);
+        IntPtr ActivateForProtocol([In] String appUserModelId, [In] IntPtr /* IShellItemArray* */itemArray, [Out] out UInt32 processId);
+    }
+
+    [ComImport, Guid("45BA127D-10A8-46EA-8AB7-56EA9078943C")]//Application Activation Manager
+    class ApplicationActivationManager : IApplicationActivationManager
+    {
+        [MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)/*, PreserveSig*/]
+        public extern IntPtr ActivateApplication([In] String appUserModelId, [In] String arguments, [In] ActivateOptions options, [Out] out UInt32 processId);
+        [MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        public extern IntPtr ActivateForFile([In] String appUserModelId, [In] [MarshalAs(UnmanagedType.Interface, IidParameterIndex = 2)]  /*IShellItemArray* */ IShellItemArray itemArray, [In] String verb, [Out] out UInt32 processId);
+        [MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        public extern IntPtr ActivateForProtocol([In] String appUserModelId, [In] IntPtr /* IShellItemArray* */itemArray, [Out] out UInt32 processId);
+    }
+
+    [ComImport]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    [Guid("43826d1e-e718-42ee-bc55-a1e261c37bfe")]
+    interface IShellItem
+    {
+    }
+
+    [ComImport]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    [Guid("b63ea76d-1f85-456f-a19c-48159efa858b")]
+    interface IShellItemArray
+    {
+    }
+
+    class ProcessManager
+    {
+        int StartProccess(string uri)
+        {
+            var mgr = new ApplicationActivationManager();
+            uint processId;
+            var name = "Microsoft.WindowsSoundRecorder_8wekyb3d8bbwe!App";
+            mgr.ActivateApplication(name, null, ActivateOptions.None, out processId);
+
+            return ((int)processId);
+        }
+
+        Boolean ProcessIsRunning(int id)
+        {
+            try
+            {
+                Process.
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
     }
 }
