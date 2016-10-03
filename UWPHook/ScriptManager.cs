04a7bb360a9a1976f46bc9964ce27c1f7b3faa22
Brian Lima <briano.dev@outlook.com>
2016-10-03 11:07:45 -0300

    Adding a settings page

--- UWPHook/ScriptManager.cs
+++ UWPHook/ScriptManager.cs
@@ -10,15 +10,12 @@ namespace UWPHook
         public string RunScript(string scriptText)
         {
             // create Powershell runspace
-
             Runspace runspace = RunspaceFactory.CreateRunspace();
 
             // open it
-
             runspace.Open();
 
             // create a pipeline and feed it the script text
-
             Pipeline pipeline = runspace.CreatePipeline();
             pipeline.Commands.AddScript(scriptText);
 
@@ -30,19 +27,15 @@ namespace UWPHook
 
             // "Get-Process" returns a collection
             // of System.Diagnostics.Process instances.
-
             pipeline.Commands.Add("Out-String");
 
             // execute the script
-
             Collection<PSObject> results = pipeline.Invoke();
 
             // close the runspace
-
             runspace.Close();
 
             // convert the script result into a single string
-
             StringBuilder stringBuilder = new StringBuilder();
             foreach (PSObject obj in results)
             {
