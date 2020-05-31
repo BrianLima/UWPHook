dd8ec6df0cbb10e24932a67610fc8e35f7f95681
Brian Lima <code@briano.dev>
2020-05-31 15:26:59 -0300

    Update version number

--- UWPHook/Properties/AssemblyInfo.cs
+++ UWPHook/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Briano")]
 [assembly: AssemblyProduct("UWPHook")]
-[assembly: AssemblyCopyright("Copyright Brian Lima ©  2016")]
+[assembly: AssemblyCopyright("Copyright Brian Lima ©  2020")]
 [assembly: AssemblyTrademark("Briano")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.5.1.0")]
-[assembly: AssemblyFileVersion("2.5.1.0")]
+[assembly: AssemblyVersion("2.6.0.0")]
+[assembly: AssemblyFileVersion("2.6.0.0")]
