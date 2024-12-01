3381c15e9b939347dfc339e63e697489e4fdba36
Dennis Ostermann <ostermann3k@gmail.com>
2024-12-01 21:18:00 +0100

    Fixed paypal url open

--- UWPHook/SettingsWindow.xaml.cs
+++ UWPHook/SettingsWindow.xaml.cs
@@ -64,27 +64,27 @@ namespace UWPHook
 
         private void Chip_Click(object sender, RoutedEventArgs e)
         {
-            System.Diagnostics.Process.Start("http://twitter.com/brianostorm");
+            OpenUrl("http://twitter.com/brianostorm");
         }
 
         private void Chip1_Click(object sender, RoutedEventArgs e)
         {
-            System.Diagnostics.Process.Start("http://github.com/brianlima");
+            OpenUrl("http://github.com/brianlima");
         }
 
         private void Chip2_Click(object sender, RoutedEventArgs e)
         {
-            System.Diagnostics.Process.Start("https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=9YPV3FHEFRAUQ");
+            OpenUrl("https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=9YPV3FHEFRAUQ");
         }
 
         private void update_button_Click(object sender, RoutedEventArgs e)
         {
-            System.Diagnostics.Process.Start("https://github.com/BrianLima/UWPHook/releases");
+            OpenUrl("https://github.com/BrianLima/UWPHook/releases");
         }
 
         private void help_button_Click(object sender, RoutedEventArgs e)
         {
-            System.Diagnostics.Process.Start("https://reddit.com/r/UWPHook/");
+            OpenUrl("https://reddit.com/r/UWPHook/");
         }
 
         private void clearAll_button_Click(object sender, RoutedEventArgs e)
@@ -98,10 +98,19 @@ namespace UWPHook
 
         private void key_Button_Click(object sender, RoutedEventArgs e)
         {
-            var url = "https://www.steamgriddb.com/profile/preferences/api";
             MessageBox.Show(messageBoxText: "You are being redirected to SteamGridDB website!\r\n" +
                 "Log-in, or create your account, go to your profile preferences and click 'Generate API Key', then paste the key back on UWPHook.", "Attention!", MessageBoxButton.OK, MessageBoxImage.Information );
-            Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+            OpenUrl("https://www.steamgriddb.com/profile/preferences/api");
+        }
+
+        private void OpenUrl(string url)
+        {
+            var psi = new ProcessStartInfo
+            {
+                FileName = url,
+                UseShellExecute = true
+            };
+            Process.Start(psi);
         }
     }
 }
